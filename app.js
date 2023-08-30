79863c481cbce94e634ac710c2d5c5915d2b7377
koheinthurako <captainthura99@gmail.com>
2023-08-30 14:01:26 +0630

    startTest

---
+++ app.js
@@ -0,0 +1,15 @@
+const stars = document.querySelectorAll(".stars i");
+
+stars.forEach((star, index1) => {
+    star.addEventListener('click', _ => {
+        // star ကို click တစ်ချက်နှိပ်လိုက်တာနဲ့ stars အကုန် loop ပြန်ပတ် condition စစ်ပြီး active class များထည့်သွားခြင်း
+        stars.forEach((star, index2) => {
+            index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+            if(star.classList.contains("active")) {
+                star.addEventListener('click', () => {
+                    star.classList.remove("active");
+                });
+            };
+        });
+    })
+});
